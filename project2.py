afe3cf3a8e78e1be2df4efe05c9b302d969800a0
Mihail Eric <mihail911@gmail.com>
2016-04-07 16:54:36 -0700

    added project utils

---
+++ project2.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from scipy.sparse import csr_matrix
+
+
+# Part 1
+def load_data():
+    pass
+    # Label.csv
+
+    # Group.csv
+
+    # Data50.csv
+
+
+if __name__ == "__main__":
+    pass
+
